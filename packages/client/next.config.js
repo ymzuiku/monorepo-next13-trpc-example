74cd5e6ed62ca8344b40207e7524aa98ff17e4ad
pillar <ymblender@gmail.com>
2023-01-24 00:43:09 +0800

    fix: add tauri

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,16 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/v1/:path*",
-        destination: "http://127.0.0.1:3001/v1/:path*",
-      },
-    ];
-  },
+const isProd = process.env.NODE_ENV === "production";
+module.exports = async (phase, { defaultConfig }) => {
+  let internalHost = null;
+  if (!isProd) {
+    const { internalIpV4 } = await import("internal-ip");
+    internalHost = await internalIpV4();
+  }
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    // Note: This experimental feature is required to use NextJS Image in SSG mode.
+    // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+    images: {
+      unoptimized: true,
+    },
+    // assetPrefix: isProd ? null : `http://${internalHost}:3000`,
+    async rewrites() {
+      return [
+        {
+          source: "/v1/:path*",
+          destination: `http://${internalHost}:3001/v1/:path*`,
+        },
+      ];
+    },
+  };
+  return nextConfig;
 };
-
-module.exports = nextConfig;
